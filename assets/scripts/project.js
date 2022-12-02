ce696c3abbd930c0ffe0d55d465d85f327630076
xertifiedloaded <certifiedloaded@yahoo.com>
2022-12-02 14:26:46 +0100

    my changes

--- assets/scripts/project.js
+++ assets/scripts/project.js
@@ -35,3 +35,17 @@ window.addEventListener("scroll", () => {
         myHeader.classList.remove("active-header")
     }
 })
+
+let homePage = document.querySelector(".home-btn")
+console.log(homePage)
+let backLink = document.querySelector(".back").href="index.html"
+homePage.addEventListener("click",()=>{
+backLink.classList.toggle("backLink")
+console.log("hi")
+})
+
+let myWork = document.querySelector(".work-btn")
+let workLink = document.querySelector(".my-work").href="project.html"
+myWork.addEventListener("click",()=>{
+    workLink.classList.add("workLink")
+})
